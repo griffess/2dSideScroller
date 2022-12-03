cd027756edd2ae4ca34159fe20d4298aff3ce2e5
Sam Griffith <griffithsamanthaerin@gmail.com>
2022-12-03 17:00:59 -0500

    video 9- draw game ui content added

--- script.js
+++ script.js
@@ -97,7 +97,19 @@ window.addEventListener('load', function () {
     }
     // ui handles score, timer, displayed info
     class UI {
-
+        constructor(game) {
+            this.game = game;
+            this.fontSize = 25;
+            this.fontFamily = 'Helvetica';
+            this.color = 'yellow';
+        }
+        draw(context) {
+            // ammo
+            context.fillStyle = this.color;
+            for (let i = 0; i < this.game.ammo; i++) {
+                context.fillRect(20 + 5 * i, 50, 3, 20);
+            }
+        }
     }
     // Game is the brain 
     class Game {
@@ -106,6 +118,7 @@ window.addEventListener('load', function () {
             this.height = height;
             this.player = new Player(this);
             this.input = new InputHandler(this);
+            this.ui = new UI(this);
             this.keys = [];
             this.ammo = 20;
             this.maxAmmo = 50;
@@ -123,6 +136,7 @@ window.addEventListener('load', function () {
         }
         draw(context) {
             this.player.draw(context);
+            this.ui.draw(context);
         }
     }
 
