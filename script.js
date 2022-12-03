79018a36057d963cf2263890bccb27707f86630a
Sam Griffith <griffithsamanthaerin@gmail.com>
2022-12-02 20:06:35 -0500

    console.logs removed from inputHandler class

--- script.js
+++ script.js
@@ -16,13 +16,11 @@ window.addEventListener('load', function() {
                 } else if (e.key === ' '){
                     this.game.player.shootTop()
                 }
-                console.log(this.game.keys);
             });
             window.addEventListener('keyup', e => {
                 if (this.game.keys.indexOf(e.key) > -1){
                     this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
                 }
-                console.log(this.game.keys);
             });
         }
     }
